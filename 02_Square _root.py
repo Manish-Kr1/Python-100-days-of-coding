2f0dbe16e06702905514d58cc8002521fe844dfe
Manish-Kr1 <manish881741@gmail.com>
2026-09-09 22:11:48 +0530

    first commit

---
+++ 02_Square _root.py
@@ -0,0 +1,14 @@
+# num = 64
+# num1 = int(input("Enter a number "))
+# sr = num1**(1/2) 
+
+# print("Square root of the number is ", sr)
+
+# Solution 2 using Math module
+
+import math
+num1 = int(input("enter a number "))
+
+sr = math.sqrt(num1)
+
+print("Square root of the number is ", sr)

a5e88407a22414d126ef7877d47e0b4484716347
Manish-Kr1 <manish881741@gmail.com>
2026-09-13 12:17:10 +0530

    first commit

---
+++ 06_Pos_Neg_zero.py
@@ -0,0 +1,12 @@
+# Program to check if a number is positive negative or Zero
+
+num = int(input("Enter a number "))
+
+if num > 0:
+    print("number is positive ")
+
+elif num < 0:
+    print("number is negative ")
+
+else:
+    print("number is 0 ")

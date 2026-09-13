fe777e9bcd062eeed1ad069ddc4e2333c3b9373d
Manish-Kr1 <manish881741@gmail.com>
2026-09-13 11:57:09 +0530

    first commit

--- 04_KM_to_miles.py
+++ 04_KM_to_miles.py
@@ -2,4 +2,4 @@ km = float(input("enter your value in kms "))
 
 mile = (0.621371)*km
 
-print(km, "kms in miles is equal to ", mile, "miles")
+print(km, "kms in miles is equal to ", mile, "miles")   
---
+++ 05_Swap_variables.py
@@ -0,0 +1,26 @@
+# x = 13
+# y = 12
+
+# temp = x
+# print("the value of temp is ", temp)
+
+# x = y
+# print("the value of x is ", x)
+
+# y = temp
+# print("the value of y is ", y)
+
+
+# solution 2 without using 3rd variable
+
+x = 12
+y = 13
+
+x, y = y, x
+
+print("the value of x is ", x)
+print("the value of y is ", y)
+
+
+
+

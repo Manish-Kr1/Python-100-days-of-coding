9be6b5056b1841b2140b0af4225dd6a7ee5e399f
Manish-Kr1 <manish881741@gmail.com>
2026-09-22 22:22:37 +0530

    first commit

---
+++ 16_Muliplication_Table.py
@@ -0,0 +1,18 @@
+num = int(input("Enter a number "))
+
+for i in range(1, 11):
+
+    print(num, " X ", i, "=", num*i )
+
+
+# solution 2 Using while loop
+
+n = int(input("Enter a number "))
+
+i = 1
+
+while i <= 10:
+
+    print(n, " X ", i, "=", n*i )
+
+    i += 1
